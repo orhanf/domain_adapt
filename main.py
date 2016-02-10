9e268b8bce8b2ccf81adfdfff872f1069f75cbf7
orhanf <orhan.firat@ceng.metu.edu.tr>
2016-02-10 02:44:39 -0500

    skeleton

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
 import numpy as np
 import theano
-import theano.tensor as tt
+from theano import tensor
 
 floatX = theano.config.floatX
 
 
-class ReverseGradient(theano.gof.Op):
-    view_map = {0: [0]}
-
-    __props__ = ('hp_lambda',)
-
-    def __init__(self, hp_lambda):
-        super(ReverseGradient, self).__init__()
-        self.hp_lambda = hp_lambda
-
-    def make_node(self, x):
-        return theano.gof.graph.Apply(self, [x], [x.type.make_variable()])
-
-    def perform(self, node, inputs, output_storage):
-        xin, = inputs
-        xout, = output_storage
-        xout[0] = xin
-
-    def grad(self, input, output_gradients):
-        return [-self.hp_lambda * output_gradients[0]]
-
-
-def init_param(inp_size, out_size, name):
-    return theano.shared(
-        np.random.randn(inp_size, out_size).astype(floatX),
-        name=_g(name, 'W'))
+def init_param(inp_size, out_size, name, scale=0.01, ortho=False):
+    if ortho and inp_size == out_size:
+        u, s, v = np.linalg.svd(np.random.randn(inp_size, inp_size))
+        W = u.astype('float32')
+    else:
+        W = scale * np.random.randn(inp_size, out_size).astype(floatX)
+    return theano.shared(W, name=_g(name, 'W'))
 
 
 def init_bias(layer_size, name):
@@ -37,34 +19,90 @@ def init_bias(layer_size, name):
                          name=_g(name, 'b'))
 
 
+def _p(p, q):
+    return '{}_{}'.format(p, q)
+
+
+class ReverseGradient(theano.gof.Op):
+    __props__ = ()
+
+    def __init__(self):
+        super(ReverseGradient, self).__init__()
+
+    def make_node(self, x):
+        x = theano.tensor.as_tensor_variable(x)
+        return theano.Apply(self, [x], [x.type()])
+
+    def perform(self, node, inputs, output_storage):
+        xin, = inputs
+        xout, = output_storage
+        xout[0] = xin
+
+    def infer_shape(self, node, i0_shapes):
+        return i0_shapes
+
+    def grad(self, inputs, output_grads):
+        return [-1. * output_grads[0]]
+
+    def R_op(self, inputs, eval_points):
+        if eval_points[0] is None:
+            return eval_points
+        return self.grad(inputs, eval_points)
+
+
+class DenseLayer(object):
+    def __init__(self, nin, dim, activ='lambda x: tensor.tanh(x)', prefix='ff',
+                 scale=0.01, ortho=False, **kwargs):
+        self.nin = nin
+        self.dim = dim
+        self.activ = activ
+        self.prefix = prefix
+        self.W = None
+        self.b = None
+
+    def init(self, add_bias=True):
+        self.W = init_param(self.nin, self.dim, _p(self.prefix, 'W'),
+                            scale=self.scale, ortho=self.ortho)
+        if add_bias:
+            self.b = init_bias(self.dim, _p(self.prefix, 'b'))
+
+    def fprop(self, state_below):
+        pre_act = tensor.dot(state_below, self.W) + \
+            (self.b if self.b is not None else 0.)
+        return eval(self.activ)(pre_act)
+
+    def get_params(self):
+        return [self.W] + ([self.b] if self.b is not None else [])
+
+
 def g_f(z, theta_f):
     for w_f, b_f in theta_f:
-        z = tt.tanh(theano.dot(z, w_f) + b_f)
+        z = tensor.tanh(theano.dot(z, w_f) + b_f)
     return z
 
 
 def g_y(z, theta_y):
     for w_y, b_y in theta_y[:-1]:
-        z = tt.tanh(theano.dot(z, w_y) + b_y)
+        z = tensor.tanh(theano.dot(z, w_y) + b_y)
     w_y, b_y = theta_y[-1]
-    z = tt.nnet.softmax(theano.dot(z, w_y) + b_y)
+    z = tensor.nnet.softmax(theano.dot(z, w_y) + b_y)
     return z
 
 
 def g_d(z, theta_d):
     for w_d, b_d in theta_d[:-1]:
-        z = tt.tanh(theano.dot(z, w_d) + b_d)
+        z = tensor.tanh(theano.dot(z, w_d) + b_d)
     w_d, b_d = theta_d[-1]
-    z = tt.nnet.sigmoid(theano.dot(z, w_d) + b_d)
+    z = tensor.nnet.sigmoid(theano.dot(z, w_d) + b_d)
     return z
 
 
 def l_y(z, y):
-    return tt.nnet.categorical_crossentropy(z, y).mean()
+    return tensor.nnet.categorical_crossentropy(z, y).mean()
 
 
 def l_d(z, d):
-    return tt.nnet.binary_crossentropy(z, d).mean()
+    return tensor.nnet.binary_crossentropy(z, d).mean()
 
 
 def _g(p, q):
@@ -82,34 +120,28 @@ def mlp_parameters(input_size, layer_sizes, name=None):
     return parameters, previous_size
 
 
-def build_model(input_size, f_layer_sizes, y_layer_sizes, d_layer_sizes,
-                hp_lambda, hp_mu):
+def build_model(xs, xt, ys, input_size, hp_lambda, hp_mu,
+                f_layer_sizes, y_layer_sizes, d_layer_sizes,
+                simple=False):
     r = ReverseGradient(hp_lambda)
 
     theta_f, f_size = mlp_parameters(input_size, f_layer_sizes, 'f')
     theta_y, _ = mlp_parameters(f_size, y_layer_sizes, 'y')
     theta_d, _ = mlp_parameters(f_size, d_layer_sizes, 'd')
 
-    xs = tt.matrix('xs')
-    xs.tag.test_value = np.random.randn(9, input_size).astype(floatX)
-    xt = tt.matrix('xt')
-    xt.tag.test_value = np.random.randn(10, input_size).astype(floatX)
-    ys = tt.ivector('ys')
-    ys.tag.test_value = np.random.randint(
-        y_layer_sizes[-1], size=9).astype(np.int32)
-
-    if approach:
-    fs = g_f(xs, theta_f)
-    e = l_y(g_y(fs, theta_y), ys) + \   #
-        l_d(g_d(r(fs), theta_d), 0) +\
-        l_d(g_d(r(g_f(xt, theta_f)), theta_d), 1)
+    if simple:
+        fs = g_f(xs, theta_f)
+        e = l_y(g_y(fs, theta_y), ys) + \
+            l_d(g_d(r(fs), theta_d), 0) + \
+            l_d(g_d(r(g_f(xt, theta_f)), theta_d), 1)
     else:
+        pass
+
     e.name = 'cost'
 
     thetas = [p for theta in theta_f + theta_y + theta_d for p in theta]
     grads = [theano.grad(e, p) for p in thetas]
 
-    import ipdb;ipdb.set_trace()
     updates = [(p, p - hp_mu * g) for p, g in zip(thetas, grads)]
     train = theano.function([xs, xt, ys], outputs=e, updates=updates)
 
@@ -118,9 +150,20 @@ def build_model(input_size, f_layer_sizes, y_layer_sizes, d_layer_sizes,
 
 def main():
     theano.config.compute_test_value = 'raise'
-    np.random.seed(1)
-    compile(input_size=2, f_layer_sizes=[3, 4], y_layer_sizes=[7, 8],
-            d_layer_sizes=[5, 6], hp_lambda=.5, hp_mu=.01)
+    xs = tensor.matrix('xs')
+    xt = tensor.matrix('xt')
+    ys = tensor.ivector('ys')
+    hp_lambda = tensor.scalar('hp_lambda')
+    hp_mu = tensor.scalar('hp_mu')
+    xs.tag.test_value = np.random.randn(9, 2).astype(floatX)
+    xt.tag.test_value = np.random.randn(10, 2).astype(floatX)
+    ys.tag.test_value = np.random.randint(8, size=9).astype(np.int32)
+    np.random.seed(4321)
+
+    build_model(xs, xt, ys, hp_lambda, hp_mu,
+                input_size=2, f_layer_sizes=[3, 4], y_layer_sizes=[7, 8],
+                d_layer_sizes=[5, 6])
 
 
-main()
+if __name__ == "__main__":
+    main()
